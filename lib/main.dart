2e4aa15dc2ecda6ed1327e09113f2de2d9db23e7
Goutham <tinglekargoutham@gmail.com>
2023-10-01 19:12:37 +0400

    added phone sign in. otp is coming but repatch taking to browser

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:driver_app/screens/wrapper/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
--- lib/screens/wrapper/authenticate/signin.dart
+++ lib/screens/wrapper/authenticate/signin.dart
@@ -1,9 +1,10 @@
-import 'package:driver_app/services/firebase_auth_api.dart';
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:driver_app/services/functions.dart';
 import 'package:driver_app/screens/loading.dart';
-import 'package:flutter/services.dart';
+
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -14,166 +15,164 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final _formKey = GlobalKey<FormState>();
-  String? phoneNumberError;
+  String? phoneNumberError, smsError;
   String phoneNumber = '+374';
   bool loading = false;
-  bool smsBox = true;
+  bool smsBox = false;
   String? smsValue;
-  final List<TextEditingController> _controllers = List.generate(4, (index) => TextEditingController());
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final inputCompleter = Completer<void>();
 
 
+  void verifyPhoneNumber()  {
+   _auth.verifyPhoneNumber(
+    phoneNumber: phoneNumber,
+    verificationCompleted: (PhoneAuthCredential credential) {
+      // Handle verification completed
+      // You are now signed in.
+    },
+    verificationFailed: (FirebaseAuthException exception) {
+      // Handle verification failed
+      print('Phone verification failed: ${exception.message}');
+    },
+    codeSent:  (String verificationId, int? resendToken) async {
+      // Update the UI - wait for the user to enter the SMS code
 
-  // AuthService authApi =  AuthService();
+      setState(() {
+        loading = false;
+        smsBox = true;
+      });
 
+      await waitForUserSMSCodeInput();
 
-  void _verifyPhoneNumber() {
-    // authApi.signInPhone(
-    //   phoneNumber,
-    //       (PhoneAuthCredential credential) {
-    //     // Handle verification completed
-    //     // You are now signed in.
-    //   },
-    //       (FirebaseAuthException exception) {
-    //     // Handle verification failed
-    //     print('Phone verification failed: ${exception.message}');
-    //   },
-    //         (String verificationId, int? resendToken) async {
-    //       // Update the UI - wait for the user to enter the SMS code
-    //
-    //           setState(() {
-    //             loading = false;
-    //             smsBox = true;
-    //           });
-    //
-    //    },
-    //
-    //       (String verificationId) {
-    //     // Handle auto retrieval timeout
-    //    // _firebaseAuth.setVerificationId(verificationId);
-    //   },
-    // );
+      print(smsValue);
+      // Create a PhoneAuthCredential with the code
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsValue!);
+
+      // Sign the user in (or link) with the credential
+     final user =  await _auth.signInWithCredential(credential);
+    print(user);
+
+    },
+    codeAutoRetrievalTimeout: (String verificationId) {
+      // Handle auto retrieval timeout
+      // _firebaseAuth.setVerificationId(verificationId);
+    },
+  );
+}
+
+  Future<void> waitForUserSMSCodeInput() async {
+    await inputCompleter.future;
   }
-
-
-
-
-
 
 
   @override
   Widget build(BuildContext context) {
-    return loading ? const Loading() : Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Sign In',
-          style: TextStyle(color: Colors.black45),
-        ),
-        backgroundColor: Colors.yellow[200],
-      ),
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              smsBox ?
-          Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                4,
-                    (index) => SizedBox(
-                  width: 50,
-                  child: TextFormField(
-                    controller: _controllers[index],
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly,
-                      LengthLimitingTextInputFormatter(1),
-                    ],
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.zero,
-                    ),
-                    onChanged: (value) {
-                      if (value.length == 1 && index < 3) {
-                        FocusScope.of(context).nextFocus();
-                      }
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '';
-                      }
-                      return null;
-                    },
-
-
-                  ),
+    return loading
+        ? const Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Sign In',
+                style: TextStyle(color: Colors.black45),
+              ),
+              backgroundColor: Colors.yellow[200],
+            ),
+            backgroundColor: Colors.white,
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    smsBox
+                        ? TextFormField(
+                            keyboardType: TextInputType.number,
+                            maxLength: 6,
+                            decoration: InputDecoration(
+                              hintText: 'Enter SMS Code',
+                              border: const OutlineInputBorder(),
+                              errorText: smsError,
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'SMS Code is required';
+                              } else if (value.length != 6) {
+                                return 'Enter a valid 6-digit code';
+                              }
+                              return null; // Return null if the validation is successful
+                            },
+                            onChanged: (value) {
+                              smsValue = value;
+                            },
+                          )
+                        : TextFormField(
+                            initialValue: phoneNumber,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              hintText: 'Phone Number',
+                              errorText: phoneNumberError,
+                            ),
+                            keyboardType: TextInputType.phone,
+                            onChanged: (value) {
+                              phoneNumber = value;
+                            },
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value == '+374') {
+                                return 'Please enter phone number';
+                              }
+                              return null;
+                            },
+                          ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      style: const ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.yellow),
+                        minimumSize: MaterialStatePropertyAll(
+                          Size(100, 30),
+                        ),
+                      ),
+                      onPressed: () async {
+                        final value = _formKey.currentState?.validate();
+                        if (value == true) {
+                          setState(() {
+                            loading = true;
+                          });
+                          if (!smsBox) {
+                            final result = await Functions()
+                                .checkPhoneNumberExists(phoneNumber);
+                            if (result == 'error' ||
+                                result == 'Phone number not registered') {
+                              setState(() {
+                                phoneNumberError = result;
+                                loading = false;
+                              });
+                            } else {
+                              // Perform login
+                               verifyPhoneNumber();
+                            }
+                          } else {
+                            inputCompleter.complete();
+                          }
+                        }
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black45),
+                      ),
+                    )
+                  ],
                 ),
               ),
-          )
-                  :
-              TextFormField(
-                initialValue: phoneNumber,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: 'Phone Number',
-                  errorText: phoneNumberError,
-                ),
-                keyboardType: TextInputType.phone,
-                onChanged: (value) {
-                  phoneNumber = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty || value == '+374') {
-                    return 'Please enter phone number';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.yellow),
-                  minimumSize: MaterialStatePropertyAll(
-                    Size(100, 30),
-                  ),
-                ),
-
-                onPressed: () async {
-                  final value = _formKey.currentState?.validate();
-                  if (value == true) {
-                    setState(() {
-                      loading = true;
-                    });
-                    if(!smsBox){
-                      final result = await Functions().checkPhoneNumberExists(phoneNumber);
-                      if (result == 'error' || result == 'Phone number not registered') {
-                        setState(() {
-                          phoneNumberError = result;
-                          loading = false;
-                        });
-                      } else {
-                        // Perform login
-                        _verifyPhoneNumber();
-                      }
-                    }else {
-
-                    }
-                  }
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.black45),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
+
+
 }
